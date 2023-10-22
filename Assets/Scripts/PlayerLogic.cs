ccc567ba5f46f4e25ea01819420372e7dc9ac86d
zac_BM <zac.b.millman@gmail.com>
2023-10-22 14:35:22 -0400

    started dash
    still very broken but at least not crashing anymore

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -10,10 +10,17 @@ public class PlayerController : MonoBehaviour
     //public HealthComponent hp;
     //public HurtboxComponent hbox;
     public MoveComponent mover;
+    public CharacterController charCon;
 
     public Vector3 movValue = Vector3.zero;
 
     public InputAction movement;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashPower = 40f;
+    private float dashTime = 0.2f;
+    private float dashCooldown = 1f;
 
     void OnEnable()
     {
@@ -32,14 +39,19 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+
         ReceiveInput();
         Movement();
+        
         //WhenDying();
     }
 
     void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     void Movement()
@@ -68,4 +80,16 @@ public class PlayerController : MonoBehaviour
         Destroy(this.gameObject);
     }
     */
+
+    private IEnumerator Dash()
+    {
+        Debug.Log("Nice");
+        canDash = false;
+        isDashing = true;       
+        Vector3 moveDirection = transform.forward * dashTime;
+        transform.position += (moveDirection * Time.deltaTime * dashPower);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
 }
