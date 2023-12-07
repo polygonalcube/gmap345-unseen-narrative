31b2c5df703b4a2f4bbffdfb7ed05a0a398f623a
Jusoboy <polygonalcube@protonmail.com>
2023-12-07 17:30:24 -0500

    Beginnings of Enemy Lancer

---
+++ Assets/Scripts/EnemyLancer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLancer : EnemyBaseClass
+{
+    public MoveComponent mover;
+
+    public float idleTime = 0f;
+
+    public Vector2 idealDist = new Vector2(3f, 5f);
+    public float[] stateTimes = new float[2];
+    public float stateTimer = 0f;
+
+    public GameObject lance;
+    float lanceAngle = 0f;
+
+    public enum States
+    {
+        IDLE,
+        MOVE
+    }
+    public States state = States.MOVE;
+
+    public override void Update()
+    {
+        base.Update();
+        if (player != null)
+        {
+            switch(state)
+            {
+                case States.IDLE:
+                    stateTimer += Time.deltaTime;
+                    if (stateTimer > stateTimes[0])
+                    {
+                        stateTimer = 0f;
+                        state = States.MOVE;
+                    }
+                    break;
+                case States.MOVE:
+                    if (stateTimer > stateTimes[1])
+                    {
+                        stateTimer = 0f;
+                        state = States.IDLE;
+                    }
+                    transform.eulerAngles = new Vector3(0f, lanceAngle, 0f);
+                    mover.MoveAngularly(transform.forward * GameManager.gm.timeSlowMulti);
+                    mover.ResetY();
+                    transform.eulerAngles = Vector3.zero;
+                    break;
+            }
+        }
+    }  
+
+    float GetMoveAng()
+    {
+        return AngleToObject(new Vector2(transform.position.x, transform.position.z), new Vector2(player.transform.position.x, player.transform.position.z));
+    } 
+}
